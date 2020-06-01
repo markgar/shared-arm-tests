0337d75c50c9ed51fd68977b0f1811f6f117383f
Mark Garner <mgarner@microsoft.com>
2020-06-01 16:15:24 +0000

    fix location test

--- test-location.tests.ps1
+++ test-location.tests.ps1
@@ -5,8 +5,8 @@ Param(
 $template = Get-Content -Path $TemplatePath | Out-String | ConvertFrom-Json
 
 
-Describe 'Sample Pester Test of an ARM Template' {
-    It 'Has a resource title of Microsoft.Network/virtualNetworks' {
-        $template.resources[0].location | Should -Be 'centralus'
+Describe 'Security Team Tests' {
+    It 'Has a resource location of eastus' {
+        $template.resources[0].location | Should -Be 'eastus'
     }
 }
